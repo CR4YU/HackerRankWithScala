9b52c6d3bab60499d0ce6e9de334ad8f940c4b5b
CR4YU <craayu@gmail.com>
2019-05-04 21:07:09 +0200

    simple array sum problem

--- src/main/scala/hackerrank/HackerRank.scala
+++ src/main/scala/hackerrank/HackerRank.scala
@@ -2,8 +2,7 @@ package hackerrank
 
 object HackerRank {
 
-
-  /*
+  /**
     * Solve me first
     * hackerrank.com/challenges/solve-me-first
     */
@@ -11,5 +10,15 @@ object HackerRank {
   val solveMeFirst2: (Int, Int) => Int = _ + _
 
 
+  /**
+    * Simple Array Sum
+    * hackerrank.com/challenges/simple-array-sum
+    */
+  def simpleArraySum(array: Array[Int]): Int = array.sum
+  def simpleArraySum2(array: Array[Int]): Int = {
+    var sum = 0
+    array.foreach(i => sum += i)
+    sum
+  }
 
 }
